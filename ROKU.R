b4b58d87a95df38afd001423c4fe88913e25a7e3
Kevin Ha <k.ha@mail.utoronto.ca>
2013-09-10 22:04:28 -0400

    - Add a small positive value to avoid NaN divisions - Handle constituitive events separately - Update build_outlier_combinations algorithm - Add main ROKU function

--- ROKU.R
+++ ROKU.R
@@ -19,46 +19,82 @@ preprocess_with_tukey <- function(x) {
   return(new.x)
 }
 
-find_tissue_outliers <- function(x) {
+find_tissue_outliers <- function(x, epsilon=1e-04) {
   # Step 3 of ROKU - finding tissue outliers
   
   # Get Z-scores
   z <- scale(x)
+#   z <- (x - median(x))/(mad(x) + epsilon)
   
   # Sort Z-scores
-  z.sorted <- sort(z)
+#   st <- order(z)
+  if (attr(z, "scaled:scale") == 0) {
+    return(rep(0, length(z)))
+  }
   
   # For each combination of U:
   #   Find best combunation of U
-  U <- rep(NA, length(z.sorted))
-  outlier.models <- build_outlier_combinations(length(z.sorted))
+  outlier.models <- build_outlier_combinations(z)
+  U <- rep(NA, length(outlier.models))
   for (i in 1:length(U)) {
-    U[i] <- U.statistic(z.sorted, outlier.models[[i]])
+    U[i] <- U.statistic(z, outlier.models[[i]])
   }
   print(outlier.models)
   print(U)
-  return(outlier.models[[which.min(U)]])
+  best.model <- outlier.models[[which.min(U)]]
+#   outliers <- rep(0,length(z))
+#   outliers[st[best.model == 1]] <- 1
+  
+  return(best.model)
 }
 
-build_outlier_combinations <- function(n) {
+build_outlier_combinations <- function(z, c=0.5) {
   obs <- list()
-  mid <- floor(n/2)
-  for (i in 1:mid) {
-    obs[[i]] <- c(rep(1, i), rep(0, n-i))
-  }
   
-  for (i in (mid+1):n) {
-    obs[[i]] <- c(rep(0, i-1), rep(1, n-i+1))
+  ix <- which(abs(z) > c)
+  
+  for (i in 1:length(ix)) {
+    comb <- combn(ix, i)
+    for (j in 1:ncol(comb)) {
+      values <- rep(0, length(z))
+      values[comb[,j]] <- 1
+      obs[[length(obs)+1]] <- values
+    }
   }
   return(obs)
 }
 
-U.statistic <- function(x, outliers) {
+U.statistic <- function(x, outliers, epsilon=1e-04) {
   stopifnot(length(outliers) == length(x))
   non.outliers <- outliers == 0
   sigma <- sd(x[non.outliers])
   n <- sum(non.outliers)
   s <- sum(outliers)
-  U <- n*log(sigma) + sqrt(2)*s*(log(factorial(n))/n)
+  U <- n*log(sigma + epsilon) + sqrt(2)*s*(log(factorial(n))/n)
   return(U)
 }
+
+ROKU <- function(m, cores=8) {
+  if (nrow(m) < 10) cores <- 2
+  
+  # pre-process
+  x.prime <- mclapply(1:nrow(m), function(i) preprocess_with_tukey(m[i,]), 
+                      mc.cores=cores)
+  x.prime <- do.call("rbind", x.prime)
+  dimnames(x.prime) <- list(rownames(m), colnames(m))
+  
+  # calculate Shannon entropy
+  H <- mclapply(1:nrow(x.prime), function(i) entropy(x.prime[i,], unit="log2"),
+                mc.cores=cores)
+  H <- unlist(H)
+  names(H) <- rownames(m)
+  
+  # find outliers
+  O <- mclapply(1:nrow(m), function(i) find_tissue_outliers(m[i,]), mc.cores=cores)
+  O <- do.call("rbind", O)
+  dimnames(O) <- list(rownames(m), colnames(m))
+  
+  return(list(entropy=H, tissue.specific.outliers=O))
+}
+
+
