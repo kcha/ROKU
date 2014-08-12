e002abdf20394cae6b0a16f6b629991e863f0a4a
Kevin Ha <k.ha@mail.utoronto.ca>
2014-08-12 16:51:51 -0400

    Updates from 2013-09-13

--- ROKU.R
+++ ROKU.R
@@ -15,42 +15,64 @@ library(parallel)
 preprocess_with_tukey <- function(x) {
   # Step 1 of ROKU
   tu <- tukey.biweight(x[!is.na(x)])
-  new.x <- abs(x - tu)
+  new.x <- x - tu
   return(new.x)
 }
 
 find_tissue_outliers <- function(x, epsilon=1e-04) {
   # Step 3 of ROKU - finding tissue outliers
-#   x <- x[!is.na(x)]
+  best.model <- list(Model=rep(0, length(x)), Outlier.Detection.Method=NA)
+  best.model[[1]][is.na(x)] <- NA
+    
+  sd <- sd(x, na.rm=T)
+  
+#   if (attr(z, "scaled:scale") == 0) return(best.model)
+  if (all(is.na(x)))
+    return(best.model)
+  if (is.na(sd)) 
+    return(best.model)
+  if (sd < epsilon)
+    return(best.model)
   
   # Get Z-scores
   z <- scale(x)
-#   z <- (x - median(x))/(mad(x) + epsilon)
+#   z <- (x - median(x, na.rm=T))/(mad(x, na.rm=T) + epsilon)
   ord <- order(z)
-  
-  best.model <- rep(0, length(z))
-  
-  # Sort Z-scores
-#   st <- order(z)
-  if (attr(z, "scaled:scale") == 0) return(best.model)
   
   # For each combination of U:
   #   Find best combunation of U
-  outlier.models <- build_outlier_combinations(z)
+  outlier.models <- build_outlier_combinations(z[ord])
   if (is.null(outlier.models)) return(best.model)
   
   U <- rep(NA, length(outlier.models))
   for (i in 1:length(U)) {
     U[i] <- U.statistic(z[ord], outlier.models[[i]])
   }
+
 #   print(outlier.models)
 #   print(U)
+#   browser()
   
   min.U <- which.min(U)
   if (length(min.U) != 0) {
     s <- outlier.models[[min.U]]
-    best.model[ord[s == 1]] <- 1
-  }
+    
+    # 2 step approach:
+    #   1) If an outlier is found, assign outlier labels to the original vector
+    #   2) If not, proceed to doing k-means clustering (k=2)
+    if (sum(s, na.rm=T) != 0) {
+      best.model[[1]][ord[s == 1]] <- 1
+      best.model[[2]] <- "AIC"
+    } else {
+      km <- kmeans(z[!is.na(z)], 2)
+      if (diff(km$size) > 0.1*length(x)) {
+        smallest.cluster <- which.min(km$size)
+        best.model[[1]][!is.na(z)][km$cluster == smallest.cluster] <- 1
+        best.model[[2]] <- "KM"
+      }
+    }
+    
+  } 
   return(best.model)
 }
 
@@ -71,6 +93,10 @@ build_outlier_combinations <- function(z) {
   }
   obs[[length(obs)+1]] <- rep(0, n)
   
+  for (i in 1:length(obs)) {
+    obs[[i]][is.na(z)] <- NA
+  }
+  
 #   ix <- which(abs(z) > c)
 #   
 #   if (length(ix) == 0) {
@@ -85,17 +111,30 @@ build_outlier_combinations <- function(z) {
 #       obs[[length(obs)+1]] <- values
 #     }
 #   }
-  return(obs)
+  return(unique(obs))
 }
 
 U.statistic <- function(x, outliers, epsilon=1e-04) {
   stopifnot(length(outliers) == length(x))
   non.outliers <- outliers == 0
   sigma <- sd(x[non.outliers], na.rm=T)
-  n <- sum(non.outliers)
-  s <- sum(outliers)
+  n <- sum(non.outliers, na.rm=T)
+  s <- sum(outliers, na.rm=T)
   U <- n*log(sigma + epsilon) + sqrt(2)*s*(log(factorial(n))/n)
   return(U)
+}
+
+entropy.shannon <- function(x, unit = c("log", "log2", "log10")) {
+  epsilon <- 1e-08
+  unit <- match.arg(unit)
+  p <- x / sum(x)
+  p <- p + epsilon
+  H <- -sum(p * log(p))
+  if (unit == "log2")
+    H <- H/log(2)
+  if (unit == "log10")
+    H <- H/log(10)
+  return(H)
 }
 
 ROKU <- function(m, cores=8) {
@@ -107,20 +146,25 @@ ROKU <- function(m, cores=8) {
                       mc.cores=cores)
   
   # calculate Shannon entropy
-  H <- mclapply(x.prime, function(i) entropy(i[!is.na(i)], unit="log2"),
+  H <- mclapply(x.prime, function(i) entropy(abs(i[!is.na(i)]), unit="log2"),
                 mc.cores=cores)
   H <- unlist(H)
   names(H) <- rownames(m)
   H.pct <- H / log2(rowSums(!is.na(m)))
-  
+ 
   # find outliers
-  O <- mclapply(1:nrow(m), function(i) find_tissue_outliers(m[i,]), mc.cores=cores)
-  O <- do.call("rbind", O)
-  dimnames(O) <- list(rownames(m), colnames(m))
+  O <- mclapply(x.prime, function(i) find_tissue_outliers(abs(i)), mc.cores=cores)
+  M <- do.call("rbind", lapply(O, "[[", 1))
+  M2 <- do.call("rbind", lapply(O, "[[", 2))
+  dimnames(M) <- list(rownames(m), colnames(m))
   
   x.prime <- do.call("rbind", x.prime)
   dimnames(x.prime) <- list(rownames(m), colnames(m))
-  return(list(entropy=H, entropy.pct=H.pct, outliers=O*x.prime))
+
+  v <- M*x.prime
+  v <- data.frame(v, Outlier.Detection.Method=M2)
+  
+  return(list(entropy=H, entropy.pct=H.pct, outliers=v))
 }
 
 ROKU.2 <- function(m, cores) {
@@ -143,8 +187,13 @@ ROKU.run <- function(x) {
   return(values)
 }
 
-ROKU.post_filter <- function(r, minDiff=15) {
-  t <- which(rowSums(r$outliers > minDiff, na.rm=T) >= 1 )
+ROKU.post_filter.index <- function(df, minDiff=15) {
+  t <- which(rowSums(df > minDiff, na.rm=T) >= 1)
+  return(t)
+}
+
+ROKU.post_filter <- function(r, ...) {
+  t <- ROKU.post_filter.index(r$outliers, ...)
   post <- list(entropy=r$entropy[t],
                entropy.pct=r$entropy.pct[t],
                outliers=r$outliers[t,])
