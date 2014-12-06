4cd19ab7ee1f5b5c4581a0fd6a4ada5937e5867e
Kevin Ha <kcha@users.noreply.github.com>
2014-12-06 16:59:08 -0500

    Initial import

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Sample genes with simulated percent spliced-in (PSI) data
+#'
+#' Contains simulated PSI data to test for tissue-specificity.
+#' 
+#' @docType data
+#' @name psidata
+#' @usage psidata
+#' @format A 8 x 10 data frame
+#' @keywords datasets
+NULL
