bf8977ceb93729f573934cfeacd74a2018d892ce
Kevin Ha <kcha@users.noreply.github.com>
2014-12-10 17:26:32 -0500

    Fix unit test

--- tests/testthat/test_u_statistic.R
+++ tests/testthat/test_u_statistic.R
@@ -4,4 +4,5 @@ test_that("U statistic calculated for n = 3", {
   x <- c(1, 2, 10)
   target <- u_statistic(x, c(0, 0, 1))
   expected <- -0.2027353
+  expect_equal(target, expected, tolerance=1e-6)
 })
