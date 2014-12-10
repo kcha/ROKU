ae8e15a68bd9e5712bfa088db1f9e8afa78dd3ab
Kevin Ha <kcha@users.noreply.github.com>
2014-12-10 16:39:11 -0500

    Fix model construction combinations

--- R/build_outlier_combinations.R
+++ R/build_outlier_combinations.R
@@ -14,15 +14,18 @@
 build_outlier_combinations <- function(z) {
   obs <- list()
   n <- length(z)
-  mid <- floor(n/2)
+  mid <- ceiling(n/2)
   for (i in 1:mid) {
     obs[[i]] <- c(rep(1, i), rep(0, n-i))
   }
   for (i in (mid+1):n) {
     obs[[i]] <- c(rep(0, i-1), rep(1, n-i+1))
   }
-  for (i in 1:(mid-1)) {
-    for (j in (mid+2):n) {
+  for (i in 1:(mid)) {
+    for (j in (mid+1):n) {
+      if (all(obs[[i]] + obs[[j]] == 1)) {
+        next;
+      } 
       obs[[length(obs)+1]] <- obs[[i]] + obs[[j]] 
     }
   }
@@ -32,19 +35,5 @@ build_outlier_combinations <- function(z) {
     obs[[i]][is.na(z)] <- NA
   }
   
-  #   ix <- which(abs(z) > c)
-  #   
-  #   if (length(ix) == 0) {
-  #     return(NULL)
-  #   }
-  #   
-  #   for (i in 1:length(ix)) {
-  #     comb <- combn(ix, i)
-  #     for (j in 1:ncol(comb)) {
-  #       values <- rep(0, length(z))
-  #       values[comb[,j]] <- 1
-  #       obs[[length(obs)+1]] <- values
-  #     }
-  #   }
   return(unique(obs))
 }
